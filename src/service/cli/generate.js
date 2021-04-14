08e594186b47f73273a5c66eb854aeb0cd624b9d
ksenia <arfloger@gmail.com>
2021-04-14 19:56:48 +0300

    решает задание Покоритель файлов

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -11,57 +11,11 @@ const {
 
 const FILE_NAME = `mocks.json`;
 
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
 
-const ANNOUNCES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`
-];
-
-const AnnounceRestrict = {
+const SentenceRestrict = {
   MIN: 1,
   MAX: 5
 };
@@ -78,16 +32,25 @@ const createDate = () => {
   return new Date(randomDate);
 };
 
-const generateCards = (count) => {
+const generateCards = (count, {titles = [], categories = [], sentences = []}) => {
   return Array(count).fill({}).map(() => ({
-    title: getRandomItem(TITLES),
+    title: getRandomItem(titles),
     createdDate: createDate(),
-    announce: getShuffleItem(ANNOUNCES, AnnounceRestrict.MIN, AnnounceRestrict.MAX).join(``),
-    fullText: getShuffleItem(ANNOUNCES).join(``),
-    category: getShuffleItem(CATEGORIES, 1, getRandomInt(2, CATEGORIES.length - 1))
+    announce: getShuffleItem(sentences, SentenceRestrict.MIN, SentenceRestrict.MAX).join(``),
+    fullText: getShuffleItem(sentences).join(``),
+    category: getShuffleItem(categories, 1, getRandomInt(2, categories.length - 1))
   }));
 };
 
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.split(`\n`);
+  } catch (err) {
+    console.log(chalk.red(err));
+    return [];
+  }
+};
 
 module.exports = {
   name: `--generate`,
@@ -100,7 +63,10 @@ module.exports = {
       process.exit(ExitCode.ERROR);
     }
 
-    const content = JSON.stringify(generateCards(countCards));
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const content = JSON.stringify(generateCards(countCards, {titles, categories, sentences}));
 
     try {
       await fs.writeFile(FILE_NAME, content);
